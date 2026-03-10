f845e3b133560fd52e54fecaaafa955979dbbd88
saranovvv <mjurgas.szkola@gmail.com>
2026-03-11 00:25:14 +0100

    pierwszy commit

---
+++ main/main.py
@@ -0,0 +1,14 @@
+import tkinter as tk
+import mysql.connector
+
+
+root = tk.Tk()
+root.title("Cash&Sum by saranov")
+root.geometry("1000x500")
+
+top_frame = tk.Frame(root, bg="red", width=1000, height=50)
+top_frame.pack()
+
+
+
+root.mainloop()
