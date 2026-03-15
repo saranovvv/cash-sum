a2316c3787b6de32de27efac1719bd03f2855212
saranovvv <mjurgas.szkola@gmail.com>
2026-03-15 14:45:05 +0100

    proby

--- main/main.py
+++ main/main.py
@@ -6,17 +6,27 @@ import ss.sum as sums
 root = tk.Tk()
 root.title("Cash&Sum by saranov")
 root.geometry("1000x500")
+root.update_idletasks()
 
-# top frame
-top_frame = header.topframe(root,1000,100,"red")
+
+tk.Grid.rowconfigure(root, 0, weight = 1)
+tk.Grid.columnconfigure(root, 0, weight = 1)
+
+
+
+top_frame = header.topframe(root,root.winfo_width(),100,"red")
 top_frame.place()
+top_frame.opcje()
+top_frame.test()
 
-# main site
 main_site = mains.main_site(root, 800, 400, "green")
 main_site.place()
 
+
+
+
+
 sum_site = sums.sum_frame(root, 200, 400, "blue")
 sum_site.place()
-
 
 root.mainloop()
--- main/top/top.py
+++ main/top/top.py
@@ -2,17 +2,24 @@ import tkinter as tk
 
 class topframe:
     def __init__(self, root, width, height, color):
-        self.root   = root
+        self.root = root
         self.height = height
         self.width = width
         self.color = color
-
-
+    
     def place(self):
         global frame
         frame = tk.Frame(self.root, height=self.height, width=self.width, bg=str(self.color))
-        frame.grid(row=0, column=0, columnspan=2, sticky="N")        
+        frame.grid(row=0, column=0, columnspan=2)
 
+    def opcje(self):
+        global opcje_frame
+        opcje_frame = tk.Frame(frame, height=self.height, width=self.height, bg="pink")
+        opcje_frame.place(x=0, y=0)
 
+    def test(self):
+        test = tk.Label(frame, text="test")
+        test.place(x=500, y=0)
    
+
      
