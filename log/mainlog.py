a243512603db0bfd2d1fe9b50cafb07dcbba76c5
Michał Jurgaś <saranov@MacBook-Air-Micha.local>
2026-03-23 00:09:09 +0100

    wyswietlanie w jednym oknie poczatek

--- log/mainlog.py
+++ log/mainlog.py
@@ -1,27 +1,13 @@
 import tkinter as tk
 import log.frames.log_frame as lf
 import log.frames.top_log_frame as tlf
-class log():    
-
-    def __init__(self):
-        pass
-
-    def startlog(self, root):
-        top_frame = tlf.top_log_frame()
-        top_frame.place(root, 100, 100)
-        pass
-        
 
 
-    # def centerwindow(self, root, width, height):
-    #     self.root = root
-    #     self.width = width
-    #     self.height = height
-        
-    #     screen_height = self.root.winfo_screenheight()
-    #     screen_width = self.root.winfo_screenwidth()
+# zrobić to na tk.Frame
+# bez tego .tkraise() nie bedzie dzialal 
 
-    #     x = (screen_width/2) - (self.width/2)
-    #     y = (screen_height/2) - (self.height/2)
-
-    #     self.root.geometry('%dx%d+%d+%d' % (self.width, screen_height, x, y))
+class log_frame(tk.Frame):  
+    def __init__(self, master):
+        super().__init__(master)
+        self.config(bg="green", width=150, height=150)
+        self.pack()
