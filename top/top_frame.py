a3909582b330dc8f3c02e8346271c8c3a59049b9
saranovvv <mjurgas.szkola@gmail.com>
2026-03-24 20:57:24 +0100

    dodanie top_frame

---
+++ top/top_frame.py
@@ -0,0 +1,9 @@
+import tkinter as tk
+
+class top_frame(tk.Frame):
+    def __init__(self, master):
+        super().__init__(master)
+        self.config()
+        
+        pass
+                 
